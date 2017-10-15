03468b1bcbf2e8f2dec7a627fbe1ad58ed4e5114
Joseph Levenson <josephlevenson@Josephs-MacBook-Pro.local>
2017-10-15 18:58:59 -0400

    Some progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
+import { Col, Row } from 'reactstrap';
 import './App.css';
 
 class App extends React.Component {
   render() {
+    const photo = require('./assets/photo.jpg');
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Stay tuned, work in progress</h2>
-        </div>
-        <p className="App-intro">
-        </p>
+        <Row>
+          <Col xs={8} className='text-center'>
+            <div className="media">
+              <img className='img-thumbnail rounded-circle' height={200} width={200} src={ photo }/>
+              <div style={{ paddingLeft: '10px' }} className="media-body align-self-center">
+                <h2 className="display-3">Hi, I'm Joe.</h2>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        
       </div>
     );
   }
